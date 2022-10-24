9b7bc5ab5e77e329f31791a769b60beff19bf710
Polo <90496416+Polo4217@users.noreply.github.com>
2022-10-24 23:10:24 +0100

    Add files via upload

---
+++ qb-spawn/server.lua
@@ -0,0 +1,15 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
+    if cid ~= nil then
+        local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
+        if houses[1] ~= nil then
+            print(json.encode(houses))
+            cb(houses)
+        else
+            cb({})
+        end
+    else
+        cb({})
+    end
+end)
